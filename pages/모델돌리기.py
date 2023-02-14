583d68a3e5916644b575a5af6916d94952e6f24e
Chu Chang Wook <80829113+chaliechu117@users.noreply.github.com>
2023-02-14 20:17:31 +0900

    Update 모델돌리기.py

--- pages/모델돌리기.py
+++ pages/모델돌리기.py
@@ -8,7 +8,10 @@ df = pd.read_pickle("data/score_df.pkl")
 st.set_page_config(layout="centered")
 st.title('변수 설정을 통해 교통사고위헙도 예측하기')
 
-feat_list = df.columns.tolist()
+#경찰청 구분
+NPA_CL_dic = {"충남청":19, "대전청":13,"세종청": 31}
+NPA = st.slider('경찰청 구분'. ['충남청','대전청','세종청'])
+location = NPA_CL_dic[NPA]
 
 
 
