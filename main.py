32353e57322411d9b73a7bcb9ddaea4ea710cf29
Chu Chang Wook <80829113+chaliechu117@users.noreply.github.com>
2023-02-14 14:55:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ st.set_page_config(layout="wide")
 # For EDA
 df = pd.read_pickle('data/score_df.pkl')
 
-st.title("Employee Future Prediction")
-st.header("LIKELION AI SCHOOL-6th, 조용한사자처럼")
+st.title("기상 별 교통사고 위험율 예측")
+st.header("추음새 조")
 st.header("About the Data")
