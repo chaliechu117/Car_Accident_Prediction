f7cd9af3a9a317bef01e31882ca94acfe25177e2
Chu Chang Wook <80829113+chaliechu117@users.noreply.github.com>
2023-02-14 23:04:57 +0900

    Update 모델돌리기.py

--- pages/모델돌리기.py
+++ pages/모델돌리기.py
@@ -18,16 +18,12 @@ col1, col2 = st.columns(2)
 with col1:
     #경찰청 구분
     feat_dict['NPA_CL'].append(st.selectbox('경찰청 구분', ['충남청','대전청','세종청']))
-
-    #사건상태 : "무조건 10으로 넣기"
     
     #습도 : 0 ~ 100
     feat_dict['습도'].append(st.slider('습도',0,100,0,1))
     
     #적설 : 0 ~ 13
     feat_dict['적설'].append(st.slider('적설',0,13,0,1))
-   
-    #RPTER_SEX : "무조건 불상"
 
     #강수량 : 0 ~ 60
     feat_dict['강수량'].append(st.slider('강수량',0,60,0,1))
@@ -55,3 +51,16 @@ st.dataframe(resp_data)
 
 NPA_CL_dic = {"충남청":19, "대전청":13,"세종청": 31}
 EVT_CL_CD_dic = {"교통사고":401,"음주운전":406,"교통불편":402,"교통위반":403,"인피도주":405,"사망.대형사고":404}
+
+test_data = pd.DataFrame(df.iloc[0]).T
+test_data.drop('score', axis = 1, inplace=True)
+test_data['RPTER_SEX'] = 3.0
+
+for a in test_data.columns[-7:]:
+    test_data[a] = feat_dict[a]
+    
+test_data['NPA_CL'] = NPA_CL_dic[feat_dict['NPA_CL'][0]]
+test_data['EVT_CL_CD'] = EVT_CL_CD_dic[feat_dict['EVT_CL_CD']][0]
+
+st.write("변환된 변수")
+st.dataframe(test_data)
