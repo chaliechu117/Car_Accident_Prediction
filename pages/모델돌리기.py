a40100e1aab32305cecf33828ff56bd24336f74c
Chu Chang Wook <80829113+chaliechu117@users.noreply.github.com>
2023-02-14 19:47:36 +0900

    Create 모델돌리기.py

---
+++ pages/모델돌리기.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+
+df = pd.read_pickle("data/score_df")
+
+st.set_page_config(layout="centered")
+st.title('변수 설정을 통해 교통사고위헙도 예측하기')
