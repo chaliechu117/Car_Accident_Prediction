38b425f6ca4caac1e3dab2143faf7d6d5f83d08a
Chu Chang Wook <80829113+chaliechu117@users.noreply.github.com>
2023-02-14 19:48:19 +0900

    Update 모델돌리기.py

--- pages/모델돌리기.py
+++ pages/모델돌리기.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import joblib
 
-df = pd.read_pickle("data/score_df")
+df = pd.read_pickle("data/score_df.pkl")
 
 st.set_page_config(layout="centered")
 st.title('변수 설정을 통해 교통사고위헙도 예측하기')
